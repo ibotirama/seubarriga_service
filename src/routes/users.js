3b45d065d3fd747e1e96eb96741933147fb77f75
Pedro Henrique <pedro.henrique1@gmail.com>
2019-01-26 09:54:32 -0300

    Changing the way we handle exceptions.

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,13 +5,13 @@ module.exports = (app) => {
     }
         
     const create = async (req, res) => {
-
-        const result = await app.services.user.save(req.body)
-        if (result.error){
-            res.status(400).json(result)
+        try {
+            const result = await app.services.user.save(req.body)
+            return res.status(201).json(result[0])
         }
-        
-        return res.status(201).json(result[0])
+        catch(err){
+            return res.status(400).json({ error: err.message })
+        }
     }
 
     return { findAll, create }
--- src/services/user.js
+++ src/services/user.js
@@ -1,23 +1,18 @@
+const ValidationError = require('../errors/ValidationError')
+
 module.exports = (app) => {
     const findAll = (filter = {}) => {
-
         return app.db('users').where(filter).select();
     }
         
     const save = async (user) => {
-        if(!user.name){
-            return { error: 'Nome é um atributo obrigatório.' }
-        }
-        if(!user.mail){
-            return { error: 'Mail é um atributo obrigatório.' }
-        }
-        if(!user.password){
-            return { error: 'Password é um atributo obrigatório.' }
-        }
+        if(!user.name) throw new ValidationError('Nome é um atributo obrigatório.')
+        if(!user.mail) throw new ValidationError('Mail é um atributo obrigatório.')
+        if(!user.password) throw new ValidationError('Password é um atributo obrigatório.')
 
         const userDb = await findAll({ mail: user.mail })
         if(userDb && userDb.length > 0){
-            return { error : 'Já existe um usuário com esse mail.'}
+            throw new ValidationError('Já existe um usuário com esse mail.')
         }
 
         return app.db('users').insert(user, '*')
