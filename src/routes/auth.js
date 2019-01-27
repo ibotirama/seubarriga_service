586b4a34a2f7044165842904e4ca4f9f2e7fe59f
Pedro Henrique <pedro.henrique1@gmail.com>
2019-01-27 11:58:46 -0300

    Test for not allow access in url protected without token and passport installation.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jwt-simple');
 const bcrypt = require('bcrypt-nodejs');
-const request = require('supertest');
+const ValidationErrror = require('../errors/ValidationError')
+
 // FIXME: Move it to a file that it's in .gitignore
 const secret = 'Segredo!';
 
@@ -8,6 +9,7 @@ module.exports = (app) => {
   const signin = (req, res, next) => {
     app.services.user.findOne({ mail: req.body.mail })
       .then((user) => {
+        if (!user) throw new ValidationErrror('User or password invalida.')
         if (bcrypt.compareSync(req.body.password, user.password)) {
           const payload = {
             id: user.id,
@@ -16,7 +18,7 @@ module.exports = (app) => {
           };
           const token = jwt.encode(payload, secret);
           res.status(200).json(token);
-        }
+        } else throw new ValidationErrror('User or password invalid.');
       })
       .catch((err) => { next(err); });
   };
--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -21,8 +21,24 @@ test('Must not login using a wrong password.', () => {
       request(app).post('/auth/signin')
         .send({ mail, password: '654321' })
         .then((res) => {
-          expect(res.status).toBe(200);
-          expect(res.body).toHaveProperty('token');
+          expect(res.status).toBe(400);
+          expect(res.body.error).toBe('User or password invalid.');
         });
     });
+});
+
+test('Must not login using user and wrong password.', () => {
+  return request(app).post('/auth/signin')
+    .send({ mail: 'mywroguser@mail.com', password: 'wrongpass' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('User or password invalid.');
+    });
+});
+
+test('Must not access a protected route without a token.', () => {
+  return request(app).get('/users')
+    .then((res) => {
+      expect(res.status).toBe(401);
+    });  
 });
